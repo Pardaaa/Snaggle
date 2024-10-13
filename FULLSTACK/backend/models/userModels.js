60f154b9ebde66ba0f951f36ba0981b9ca3ee1c3
Pardaaa <rae.hutagalung@gmail.com>
2024-10-13 20:00:15 +0700

    Setup Awal

---
+++ FULLSTACK/backend/models/userModels.js
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+import db from "../config/database";
+
+const { DataTypes } = Sequelize;
+
+const user = db.define(
+  "users",
+  {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING(60),
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+export default user;
+async () => {
+  await db.sync;
+};
