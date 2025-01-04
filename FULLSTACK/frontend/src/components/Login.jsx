a1d27c6129546e10a41c6d5d6e8fa6e86fa54bfa
lalanabiyaa <octavianinabilla123@gmail.com>
2025-01-04 21:20:05 +0700

    Tampilan Login

--- FULLSTACK/frontend/src/components/Login.jsx
+++ FULLSTACK/frontend/src/components/Login.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { LoginUser, reset } from '../features/authSlice';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import photo from '../Images/background.png';
 
 const Login = () => {
    const [email, setEmail] = useState('');
    const [password, setPassword] = useState('');
+   const [showPassword, setShowPassword] = useState(false);
    const dispatch = useDispatch();
    const navigate = useNavigate();
    const { user, isError, isSuccess, isLoading, message } = useSelector(
@@ -25,68 +27,134 @@ const Login = () => {
       dispatch(LoginUser({ email, password }));
    };
 
-   // Backgroud
+   // Background
    const backgroundImage = {
       backgroundImage: `url(${photo})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       minHeight: '100vh',
    };
+
    return (
-      <section
-         className="hero  is-fullheight is-fullwidth"
-         style={backgroundImage}
-      >
-         <div className="hero-body">
-            <div className="container">
-               <div className="columns is-centered">
-                  <div className="column is-4">
-                     <form onSubmit={Auth} className="box">
-                        {isError && (
-                           <p className="has-text-centered">{message}</p>
-                        )}
-                        <div className="field">
-                           <h1 className="title is-4 has-text-centered">
-                              Login
-                           </h1>
-                           <label className="label">Email</label>
-                           <div className="control">
-                              <input
-                                 type="text"
-                                 className="input"
-                                 value={email}
-                                 onChange={e => setEmail(e.target.value)}
-                                 placeholder="Email"
-                              />
-                           </div>
-                        </div>
-                        <div className="field">
-                           <label className="label">Password</label>
-                           <div className="control">
-                              <input
-                                 type="password"
-                                 className="input"
-                                 value={password}
-                                 onChange={e => setPassword(e.target.value)}
-                                 placeholder="*******"
-                              />
-                           </div>
-                        </div>
-                        <div className="field mt-5">
-                           <button
-                              type="submit"
-                              className="button is-success is-fullwidth"
-                           >
-                              {isLoading ? 'Loading...' : 'Login'}
-                           </button>
-                        </div>
-                     </form>
+      <section style={backgroundImage}>
+         <div style={containerStyle}>
+            <div style={formContainerStyle}>
+               <form onSubmit={Auth} style={formStyle}>
+                  {isError && <p style={errorMessageStyle}>{message}</p>}
+                  <h1 style={titleStyle}>Login</h1>
+                  <div style={fieldStyle}>
+                     <input
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder="Email"
+                        style={inputStyle}
+                     />
                   </div>
-               </div>
+                  <div style={fieldStyle}>
+                     <div style={{ position: 'relative' }}>
+                        <input
+                           type={showPassword ? 'text' : 'password'}
+                           value={password}
+                           onChange={e => setPassword(e.target.value)}
+                           placeholder="Password"
+                           style={inputStyle}
+                        />
+                        <span
+                           onClick={() => setShowPassword(!showPassword)}
+                           style={iconStyle}
+                        >
+                           {showPassword ? <FaEyeSlash /> : <FaEye />}
+                        </span>
+                     </div>
+                  </div>
+                  <div style={{ textAlign: 'center' }}>
+                     <button type="submit" style={buttonStyle}>
+                        {isLoading ? 'Loading...' : 'Login'}
+                     </button>
+                  </div>
+               </form>
             </div>
          </div>
       </section>
    );
+};
+
+const containerStyle = {
+   display: 'flex',
+   justifyContent: 'center',
+   alignItems: 'center',
+   height: '100vh',
+   fontFamily: "'Jersey 25', sans-serif",
+};
+
+const formContainerStyle = {
+   width: '100%',
+   maxWidth: '420px',
+   padding: '2rem',
+   backgroundColor: 'white',
+   borderRadius: '1rem',
+   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+};
+
+const formStyle = {
+   display: 'flex',
+   flexDirection: 'column',
+   fontFamily: "'Jersey 25', sans-serif",
+};
+
+const titleStyle = {
+   textAlign: 'center',
+   fontFamily: "'Jersey 25', sans-serif",
+   fontSize: '2.7rem',
+   marginBottom: '1rem',
+   fontWeight: 'bold',
+};
+
+const fieldStyle = {
+   marginBottom: '1.5rem',
+};
+
+const inputStyle = {
+   fontFamily: "'Josefin Sans', sans-serif",
+   fontWeight: 'bold',
+   backgroundColor: '#D9D9D9',
+   boxShadow: 'inset 0 4px 8px rgba(0, 0, 0, 0.2)',
+   padding: '1rem',
+   borderRadius: '0.5rem',
+   fontSize: '1.3rem',
+   width: '100%',
+   border: 'none',
+};
+
+const iconStyle = {
+   position: 'absolute',
+   top: '50%',
+   right: '1rem',
+   transform: 'translateY(-50%)',
+   cursor: 'pointer',
+   fontSize: '1.3rem',
+   color: '#333',
+};
+
+const errorMessageStyle = {
+   textAlign: 'center',
+   fontFamily: "'Josefin Sans', sans-serif",
+   fontWeight: 'bold',
+   color: 'red',
+   marginBottom: '1rem',
+};
+
+const buttonStyle = {
+   backgroundColor: '#416B39',
+   color: 'white',
+   padding: '0.5rem 1rem',
+   borderRadius: '0.5rem',
+   boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+   fontWeight: 'bold',
+   fontSize: '1.3rem',
+   width: '35%',
+   display: 'inline-block',
 };
 
 export default Login;
